dd85edc37befe01338cfc0990f9df8eab023fa6e
www-data <www-data@ct-web02.lxd>
2020-06-16 01:57:41 +0000

    Initial commit

---
+++ assets/plugins/closenav.js
@@ -0,0 +1,8 @@
+// Check if navbar open, when link clicked, collapse navbar
+
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a:not(".dropdown-toggle")') ) {
+        $(this).collapse('hide');
+    }
+});
+// JavaScript Document
