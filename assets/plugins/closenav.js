6dcae6647fde55dd66fade5a49f9b83b7f57165d
WordPress <admin@chanenterprises.xyz>
2022-11-29 19:33:37 +0100

    Scripted auto-commit on change (2022-11-29 19:33:37) by gitwatch.sh

--- assets/plugins/closenav.js
+++
@@ -1,8 +0,0 @@
-// Check if navbar open, when link clicked, collapse navbar
-
-$(document).on('click','.navbar-collapse.in',function(e) {
-    if( $(e.target).is('a:not(".dropdown-toggle")') ) {
-        $(this).collapse('hide');
-    }
-});
-// JavaScript Document
